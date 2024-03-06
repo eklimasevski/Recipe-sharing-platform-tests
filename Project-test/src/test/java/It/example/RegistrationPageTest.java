1e9d0be2886e5a0d849274808cee6145dcb65b07
Ernest <ernest.klimasevski@gmail.com>
2024-03-06 14:38:06 +0200

    Abbrevation of the test

--- Project-test/src/test/java/It/example/RegistrationPageTest.java
+++ Project-test/src/test/java/It/example/RegistrationPageTest.java
@@ -2,28 +2,25 @@ package It.example;
 
 import org.example.RegistrationPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationPageTest extends BasePageTest {
-
     RegistrationPage registrationPage;
+    String email = "Testukas123@gmail.com";
+    String password = "Testukas123!";
+    String displayName = "Test";
+    String name = "Testas";
+    String lastName = "Testukas";
+    String gender = "Male";
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/WrongEmails")
-    void emailInputstest(String input) {
-
-        String password = "Testukas123!";
-        String displayName = "Test";
-        String name = "Testas";
-        String lastName = "Testukas";
-        String maleSelector = "Male";
-
+    public void registrationSteps(
+            String email, String password, String displayName, String name, String lastName, String gender) {
         registrationPage = new RegistrationPage(driver);
-
         registrationPage.clickRegistrationButtonInNav();
-        registrationPage.enterEmail(input);
+        registrationPage.enterEmail(email);
         registrationPage.enterPassword(password);
         registrationPage.enterConfirmPassword(password);
         registrationPage.enterDisplayName(displayName);
@@ -31,11 +28,17 @@ public class RegistrationPageTest extends BasePageTest {
         registrationPage.enterLastName(lastName);
         registrationPage.clickOnGenderSelector();
         Select select = new Select(registrationPage.getGenderSelector());
-        select.selectByVisibleText(maleSelector);
+        select.selectByVisibleText(gender);
         registrationPage.clickOnSubmitButton();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/WrongEmails")
+    void emailInputstest(String input) {
+
+        registrationSteps(input, password, displayName, name, lastName, gender);
 
         String errorMessage = registrationPage.emailInputErrorMessageText();
-
         Assertions.assertNotNull(errorMessage, "Turėjo parodyti klaidą");
     }
 
@@ -43,28 +46,9 @@ public class RegistrationPageTest extends BasePageTest {
     @CsvFileSource(resources = "/WrongPasswords")
     void passwordInputsTest(String input) {
 
-        String email = "Testukas123@gmail.com";
-        String displayName = "Test";
-        String name = "Testas";
-        String lastName = "Testukas";
-        String maleSelector = "Male";
-
-        registrationPage = new RegistrationPage(driver);
-
-        registrationPage.clickRegistrationButtonInNav();
-        registrationPage.enterEmail(email);
-        registrationPage.enterPassword(input);
-        registrationPage.enterConfirmPassword(input);
-        registrationPage.enterDisplayName(displayName);
-        registrationPage.enterFirstName(name);
-        registrationPage.enterLastName(lastName);
-        registrationPage.clickOnGenderSelector();
-        Select select = new Select(registrationPage.getGenderSelector());
-        select.selectByVisibleText(maleSelector);
-        registrationPage.clickOnSubmitButton();
+        registrationSteps(email, input, displayName, name, lastName, gender);
 
         String errorMessage = registrationPage.passswordInputErrorMessageText();
-
         Assertions.assertNotNull(errorMessage, "Turėjo parodyti klaidą");
     }
 
@@ -72,28 +56,9 @@ public class RegistrationPageTest extends BasePageTest {
     @CsvFileSource(resources = "/WrongDisplayNames")
     void displayNameInputsTest(String input) {
 
-        String email = "Testukas123@gmail.com";
-        String password = "Testukas123!";
-        String name = "Test";
-        String lastName = "Testukas";
-        String maleSelector = "Male";
-
-        registrationPage = new RegistrationPage(driver);
-
-        registrationPage.clickRegistrationButtonInNav();
-        registrationPage.enterEmail(email);
-        registrationPage.enterPassword(password);
-        registrationPage.enterConfirmPassword(password);
-        registrationPage.enterDisplayName(input);
-        registrationPage.enterFirstName(name);
-        registrationPage.enterLastName(lastName);
-        registrationPage.clickOnGenderSelector();
-        Select select = new Select(registrationPage.getGenderSelector());
-        select.selectByVisibleText(maleSelector);
-        registrationPage.clickOnSubmitButton();
+        registrationSteps(email, password, input, name, lastName, gender);
 
         String errorMessage = registrationPage.displayNameInputErrorMessageText();
-
         Assertions.assertNotNull(errorMessage, "Turėjo parodyti klaidą");
     }
 
@@ -101,28 +66,29 @@ public class RegistrationPageTest extends BasePageTest {
     @CsvFileSource(resources = "/WrongFirstNames")
     void firstNameInputsTest(String input) {
 
-        String email = "Testukas123@gmail.com";
-        String password = "Testukas123!";
-        String displayName = "Testas";
-        String lastName = "Testukas";
-        String maleSelector = "Male";
-
-        registrationPage = new RegistrationPage(driver);
-
-        registrationPage.clickRegistrationButtonInNav();
-        registrationPage.enterEmail(email);
-        registrationPage.enterPassword(password);
-        registrationPage.enterConfirmPassword(password);
-        registrationPage.enterDisplayName(displayName);
-        registrationPage.enterFirstName(input);
-        registrationPage.enterLastName(lastName);
-        registrationPage.clickOnGenderSelector();
-        Select select = new Select(registrationPage.getGenderSelector());
-        select.selectByVisibleText(maleSelector);
-        registrationPage.clickOnSubmitButton();
+        registrationSteps(email, password, displayName, input, lastName, gender);
 
         String errorMessage = registrationPage.firstnameInpurerrorMessagetext();
-
         Assertions.assertNotNull(errorMessage, "Turėjo parodyti klaidą");
+    }
+
+    @Test
+    public void emailErrorMessageThenFieldEmptyTest() {
+        String expectedError = "Email is required!";
+
+        registrationSteps("", password, displayName, name, lastName, gender);
+
+        String actualError = registrationPage.emailInputErrorMessageText();
+        Assertions.assertEquals(expectedError, actualError);
+    }
+
+    @Test
+    public void emailErrorMessageWithDoubleAt() {
+        String expectedError = "Invalid email address!";
+
+        registrationSteps("Test@@gmail.com", password, displayName, name, lastName, gender);
+
+        String actualError = registrationPage.emailInputErrorMessageText();
+        Assertions.assertEquals(expectedError, actualError);
     }
 }
