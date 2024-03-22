91406ba86e64dcb3469ae4efcde4bc219d50fe73
Ernest <ernest.klimasevski@gmail.com>
2024-03-22 13:06:07 +0200

    Add tests for categories page

--- Project-test/src/main/java/org/example/CategoriePage.java
+++ Project-test/src/main/java/org/example/CategoriePage.java
@@ -3,31 +3,138 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class CategoriePage extends BasePage {
 
     @FindBy(linkText = "Categories")
     WebElement categoriesButtonInNav;
 
-    @FindBy(xpath = "//div[@class='table']/tr/th")
+    @FindBy(linkText = "Login")
+    WebElement loginButtonInNav;
+
+    @FindBy(css = "form > .btn.btn-primary")
+    WebElement buttonToLogin;
+
+    @FindBy(css = "tr > th")
     List<WebElement> allList;
+
+    @FindBy(css = "input#email")
+    WebElement emailInput;
+
+    @FindBy(css = "input#password")
+    WebElement passwordInput;
+
+    @FindBy(css = "th > .btn.btn-primary")
+    WebElement addCategoryButton;
+
+    @FindBy(css = "tr:nth-of-type(2)")
+    WebElement secondCategoryInList;
+
+    @FindBy(css = "input#disabledTextInput")
+    WebElement addCategoryInput;
+
+    @FindBy(css = ".modal-dialog.modal-dialog-centered .btn.btn-primary")
+    WebElement addButton;
+
+    @FindBy(css = "div[role='alert']")
+    WebElement addedCategoryAlert;
+
+    @FindBy(css = ".btn.btn-secondary")
+    WebElement closeButton;
+
+    @FindBy(css = ".invalid-feedback")
+    WebElement errorMessage;
 
     public CategoriePage(WebDriver driver) {
         super(driver);
     }
 
     public void clickCategoriesInNav() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(categoriesButtonInNav));
         categoriesButtonInNav.click();
     }
 
-    public boolean searchText(String searchText) {
+    public boolean searchingText(String searchingText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(secondCategoryInList));
+
         for (WebElement element : allList) {
-            if (element.getText().equals(searchText)) {
+            if (element.getText().equals(searchingText)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public void waitForGetUrl(String url) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains(url));
+    }
+
+    public void clickLoginButtonInNav() {
+        loginButtonInNav.click();
+    }
+
+    public void clickButtonToLogin() {
+        buttonToLogin.click();
+    }
+
+    public void enterEmail(String email) {
+        emailInput.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void clickAddCategoriesButton() {
+        addCategoryButton.click();
+    }
+
+    public void enterCategory(String name) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(addCategoryInput));
+
+        addCategoryInput.sendKeys(name);
+    }
+
+    public void clickAddButton() {
+        addButton.click();
+    }
+
+    public WebElement checkAlert() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(addedCategoryAlert));
+
+        return addedCategoryAlert;
+    }
+
+    public void clickClosebutton() {
+        closeButton.click();
+    }
+
+    public static String generateCategory() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < 5; i++) {
+            char c = chars.charAt(random.nextInt(chars.length()));
+            sb.append(c);
+        }
+
+        String category = String.valueOf(sb);
+        return category.substring(0, 1).toUpperCase() + category.substring(1);
+    }
+
+    public WebElement getErrorMessage() {
+        return errorMessage;
     }
 }
--- Project-test/src/test/java/It/example/CategoriesPageTest.java
+++ Project-test/src/test/java/It/example/CategoriesPageTest.java
@@ -1,6 +1,8 @@
 package It.example;
 
 import org.example.CategoriePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -8,12 +10,71 @@ public class CategoriesPageTest extends BasePageTest {
 
     CategoriePage categoriePage;
 
+    String email = "Testukas1@gmail.com";
+    String password = "Testukas123!";
+
+    String category = CategoriePage.generateCategory();
+
+    public void login(String email, String password) {
+        categoriePage = new CategoriePage(driver);
+        categoriePage.clickLoginButtonInNav();
+        categoriePage.enterEmail(email);
+        categoriePage.enterPassword(password);
+        categoriePage.clickButtonToLogin();
+        categoriePage.clickCategoriesInNav();
+    }
+
+    public void addCategory(String category) {
+        categoriePage = new CategoriePage(driver);
+        categoriePage.clickLoginButtonInNav();
+        categoriePage.enterEmail(email);
+        categoriePage.enterPassword(password);
+        categoriePage.clickButtonToLogin();
+        categoriePage.clickCategoriesInNav();
+        categoriePage.clickAddCategoriesButton();
+        categoriePage.enterCategory(category);
+        categoriePage.clickAddButton();
+    }
+
     @ParameterizedTest
     @CsvFileSource(resources = "/Categories")
-    public void addCategorieTest(String input) {
-        categoriePage = new CategoriePage(driver);
+    public void checkCategoriesTest(String input) {
 
-        categoriePage.clickCategoriesInNav();
-        categoriePage.searchText(input);
+        login(email, password);
+
+        boolean isTextFound = categoriePage.searchingText(input);
+        Assertions.assertTrue(isTextFound);
+    }
+
+    @Test
+    public void addCategoryTest() {
+
+        addCategory(category);
+
+        Assertions.assertTrue(categoriePage.checkAlert().isDisplayed());
+
+        categoriePage.clickClosebutton();
+        boolean isTextFound = categoriePage.searchingText(category);
+        Assertions.assertTrue(isTextFound);
+    }
+
+    @Test
+    public void validationTextLessThan4SymbolsTest() {
+        String expected = "Category names must be at least 4 characters long";
+
+        addCategory("sad");
+
+        String actual = categoriePage.getErrorMessage().getText();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void validationTextwhenInputEmptyTest() {
+        String expected = "Please enter category name";
+
+        addCategory("");
+
+        String actual = categoriePage.getErrorMessage().getText();
+        Assertions.assertEquals(expected, actual);
     }
 }
